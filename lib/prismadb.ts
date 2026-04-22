f7e5397d7f3f1c64a19eaf33bf44fc5f076ac259
Suchanda Kundu <suchanda88@gmail.com>
2026-04-22 17:56:00 +1000

    fix: prisma 7 setup with config and generated client

--- lib/prismadb.ts
+++ lib/prismadb.ts
@@ -1,21 +1,11 @@
-import { PrismaClient } from "@prisma/client";
-
-import { PrismaPg } from "@prisma/adapter-pg";
-
+import { PrismaClient } from "./generated/prisma/client";
 
 declare global {
   var prisma: PrismaClient | undefined;
 };
 
-
-const connectionString = process.env.DATABASE_URL!;
-const adapter = new PrismaPg({ connectionString });
-
-
-const prismadb = globalThis.prisma || new PrismaClient({ adapter });
-
+const prismadb = globalThis.prisma || new PrismaClient();
 
 if (process.env.NODE_ENV !== "production") globalThis.prisma = prismadb;
-
 
 export default prismadb;
