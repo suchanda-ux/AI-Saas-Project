df30bce15734e9d9bf8ce9cce9be8853d05eb8b2
Suchanda Kundu <suchanda88@gmail.com>
2026-04-22 16:55:34 +1000

    Fix Prisma config (use prisma-client-js)

--- lib/prismadb.ts
+++ lib/prismadb.ts
@@ -1,15 +1,21 @@
-import { PrismaClient } from "./generated/prisma/client";
+import { PrismaClient } from "@prisma/client";
+
 import { PrismaPg } from "@prisma/adapter-pg";
+
 
 declare global {
   var prisma: PrismaClient | undefined;
 };
 
+
 const connectionString = process.env.DATABASE_URL!;
 const adapter = new PrismaPg({ connectionString });
 
+
 const prismadb = globalThis.prisma || new PrismaClient({ adapter });
 
+
 if (process.env.NODE_ENV !== "production") globalThis.prisma = prismadb;
+
 
 export default prismadb;
