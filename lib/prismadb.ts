4884c6928cc8a371fce380c40832d0ac5d2f86ac
Suchanda Kundu <suchanda88@gmail.com>
2026-04-22 18:18:47 +1000

    fix: suppress PrismaClient ts error

--- lib/prismadb.ts
+++ lib/prismadb.ts
@@ -4,6 +4,7 @@ declare global {
   var prisma: PrismaClient | undefined;
 };
 
+// @ts-ignore
 const prismadb = globalThis.prisma || new PrismaClient();
 
 if (process.env.NODE_ENV !== "production") globalThis.prisma = prismadb;
